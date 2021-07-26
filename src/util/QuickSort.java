31aad843dc533d44b32c01682814589798f3644d
Cesar <cesar@Cesars-MBA.local>
2021-07-26 16:26:31 -0500

    progress

--- src/util/QuickSort.java
+++ src/util/QuickSort.java
@@ -1,4 +1,17 @@
 package util;
 
 public class QuickSort {
+
+    public static void main(String... args) {
+
+        int[] arr = {12, 1, 34, 5, 23, 67, 43, 15, 5, 8, 4, 87};
+    }
+
+    static void callQuickSort(int[] arr) {
+        quicksort(arr, 0, arr.length - 1);
+    }
+
+    static int[] quicksort(int[] arr, int l, int r) {
+        return null;
+    }
 }
